64e310c12eeb4bb2d4ce9cd16c33efb64182ebb2
Matias Borba <85084252+19Mega@users.noreply.github.com>
2023-08-10 21:14:23 +0000

    update spotify player

--- src/js/component/spotify.js
+++ src/js/component/spotify.js
@@ -1,21 +1,21 @@
-import React, {useState, useEffect} from "react";//1. importar el hook useState
+import React, {useState, useEffect, useRef} from "react";//1. importar el hook useState
 
-
-const SongList = (num, songName, songUrl) => {
-    return(
-        <li > </li>
-    )
-}
-
-
-
-
-//create your first component
 const Spotify = () => {
 
     //REACT
     const [song,setSong]=useState([])
-    console.log(song);
+    const inputElement = useRef();
+    const [cecilia, setCecilia] = useState()
+
+    const audio = new Audio(cecilia);
+
+
+    const focusInput = () => {
+      inputElement.current.focus();
+      console.log("prueba: ", inputElement.current)
+    };
+
+
 
 
     function obtenerInfo() {
@@ -25,48 +25,62 @@ const Spotify = () => {
         .catch((error)=>console.log(error))// si hay un error me muestra cual fue
     }
 
-    console.log(song)
-
-    // useEffect(funcion anonima,array vacio)
     useEffect(function () {// onload => ejecutar codigo ni bien cargue el componente
         obtenerInfo()
     },[])
 
+    function playSong(url){      
+        setCecilia("https://assets.breatheco.de/apis/sound/" + url) // demora un cachito en setearlo
+    }
 
-    function playSong(url){
-        console.log("https://assets.breatheco.de/apis/sound/" + url)
+    function reproducir() {
+        audio.play()
+        audio.volume = 0.3
+    }
+    
+    function parar(){
+        audio.pause()
     }
 
     return (
         <div className="mt-5">
 
+            <input type="text" ref={inputElement} />
+            <button onClick={focusInput}>Focus Input</button>
+            <br/>
 
             <div className="d-flex justify-content-center">
 
-                <ol className="bg-black list-group" style={{width:"400px", height:"300px", overflowY:"scroll"}}>
-                    {song.map(function(item){return <li key={item.id} style={{color:"white"}} onClick={function(){playSong(item.url)}}>  {item.name}  </li>
-                    })}                                                                         
+                <ol className="list-group" style={{width:"400px", height:"300px", overflowY:"scroll", backgroundColor:"black"}}>
+                    {song.map(function(item){return <li key={item.id} style={{color:"white", backgroundColor:"black"}} onClick={function(){playSong(item.url)}} className="my-2 list-group-item list-group-item-action list-group-item-dark">{item.id} -  {item.name} -</li>
+                    })}                                                                     
                 </ol>
             </div>
 
-            <div class="d-flex justify-content-center mt-3">
+            <div className="d-flex justify-content-center mt-3">
                 
-                <button type="button" class="btn btn-secondary btn-sm d-flex align-items-center me-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-skip-start-fill" viewBox="0 0 16 16">
+                <button type="button" className="btn btn-secondary btn-sm d-flex align-items-center me-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-skip-start-fill" viewBox="0 0 16 16">
                         <path d="M4 4a.5.5 0 0 1 1 0v3.248l6.267-3.636c.52-.302 1.233.043 1.233.696v7.384c0 .653-.713.998-1.233.696L5 8.752V12a.5.5 0 0 1-1 0V4z"/>
                     </svg>  
                 </button>
 
-                <button type="button" class="btn btn-primary btn-sm d-flex align-items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                <button type="button" className="btn btn-primary btn-sm d-flex align-items-center" onClick={reproducir}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-play-fill" viewBox="0 0 16 16">
                         <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
                     </svg>
                 </button>
 
-                <button type="button" class="btn btn-secondary btn-sm d-flex align-items-center ms-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-skip-end-fill" viewBox="0 0 16 16">
+                <button type="button" className="btn btn-secondary btn-sm d-flex align-items-center ms-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-skip-end-fill" viewBox="0 0 16 16">
                         <path d="M12.5 4a.5.5 0 0 0-1 0v3.248L5.233 3.612C4.713 3.31 4 3.655 4 4.308v7.384c0 .653.713.998 1.233.696L11.5 8.752V12a.5.5 0 0 0 1 0V4z"/>
                     </svg>
+                </button>
+
+                <button type="button" className="btn btn-secondary btn-sm d-flex align-items-center ms-2" onClick={parar}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-square-fill" viewBox="0 0 16 16">
+                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z"/>
+                </svg>
                 </button>
 
             </div>
