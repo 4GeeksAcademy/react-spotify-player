7d72b2c1f67a5d2789e2aa72ade283f3dd3fa6af
Matias Borba <85084252+19Mega@users.noreply.github.com>
2023-08-10 18:47:06 +0000

    update spotify player

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Spotify from "./spotify";
 
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -10,6 +11,7 @@ const Home = () => {
 		<div className="container">
 			
 			<Spotify/>
+			
 			
 		</div>
 	);
--- src/js/component/spotify.js
+++ src/js/component/spotify.js
@@ -1,6 +1,15 @@
 import React, {useState, useEffect} from "react";//1. importar el hook useState
 
 
+const SongList = (num, songName, songUrl) => {
+    return(
+        <li > </li>
+    )
+}
+
+
+
+
 //create your first component
 const Spotify = () => {
 
@@ -20,68 +29,47 @@ const Spotify = () => {
 
     // useEffect(funcion anonima,array vacio)
     useEffect(function () {// onload => ejecutar codigo ni bien cargue el componente
-    //bloque de codigo que queremos ejecutar
-    //   bienvenida();
         obtenerInfo()
     },[])
 
-    // useEffect(function () {// observador de estado => ejecutar codigo si un estado que vigilemos cambia el componente
-    //     //bloque de codigo que queremos ejecutar
-    //     if (count !=0) {
-    //         notificacion();
-    //     }
-    //     },[count])
+
+    function playSong(url){
+        console.log("https://assets.breatheco.de/apis/sound/" + url)
+    }
 
     return (
-        <div className="text-center">
-
-            {/* <h1 className="text-center mt-5">Seguidores {count}</h1>
-            <p>
-                <img src={rigoImage} />
-            </p>
-                                {/*EN HTML ERA ASI =====> onclick="nombreFuncion()" 
-            <button href="#" className="btn btn-success"  onClick={handleCount}>
-                {text} <i className="fa fa-arrow-right"></i>
-            </button>
-            <ul>
-                {/* [<li>Morty Smith</li>,<li>Rick Sanchez</li> ]
-                {song.map(function(item,index){return <li key={item.id}>{item.name}</li>})}
-                {/* <li>item 1</li>
-                <li>item 2</li>
-                <li>item 3</li> 
-            </ul> */}
-            
-              
-            <ol className="justify-content-center bg-black">
-                    {song.map(function(item){return <li key={item.id} style={{color: "white", width:"100px"}}> {item.name}</li>})}
-            </ol>
+        <div className="mt-5">
 
 
-            
-            <div class="d-flex justify-content-center">
+            <div className="d-flex justify-content-center">
+
+                <ol className="bg-black list-group" style={{width:"400px", height:"300px", overflowY:"scroll"}}>
+                    {song.map(function(item){return <li key={item.id} style={{color:"white"}} onClick={function(){playSong(item.url)}}>  {item.name}  </li>
+                    })}                                                                         
+                </ol>
+            </div>
+
+            <div class="d-flex justify-content-center mt-3">
                 
-
-
                 <button type="button" class="btn btn-secondary btn-sm d-flex align-items-center me-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-skip-start-fill" viewBox="0 0 16 16">
-                    <path d="M4 4a.5.5 0 0 1 1 0v3.248l6.267-3.636c.52-.302 1.233.043 1.233.696v7.384c0 .653-.713.998-1.233.696L5 8.752V12a.5.5 0 0 1-1 0V4z"/>
-                </svg>  
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-skip-start-fill" viewBox="0 0 16 16">
+                        <path d="M4 4a.5.5 0 0 1 1 0v3.248l6.267-3.636c.52-.302 1.233.043 1.233.696v7.384c0 .653-.713.998-1.233.696L5 8.752V12a.5.5 0 0 1-1 0V4z"/>
+                    </svg>  
                 </button>
 
                 <button type="button" class="btn btn-primary btn-sm d-flex align-items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
-                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
-                </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                        <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+                    </svg>
                 </button>
 
                 <button type="button" class="btn btn-secondary btn-sm d-flex align-items-center ms-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-skip-end-fill" viewBox="0 0 16 16">
-                    <path d="M12.5 4a.5.5 0 0 0-1 0v3.248L5.233 3.612C4.713 3.31 4 3.655 4 4.308v7.384c0 .653.713.998 1.233.696L11.5 8.752V12a.5.5 0 0 0 1 0V4z"/>
-                </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-skip-end-fill" viewBox="0 0 16 16">
+                        <path d="M12.5 4a.5.5 0 0 0-1 0v3.248L5.233 3.612C4.713 3.31 4 3.655 4 4.308v7.384c0 .653.713.998 1.233.696L11.5 8.752V12a.5.5 0 0 0 1 0V4z"/>
+                    </svg>
                 </button>
 
             </div>
-
 
         </div>
     );
